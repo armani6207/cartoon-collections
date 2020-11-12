0c24e7fb5e46fa27adee90fefac48c8d867b9788
Armando <rojas.armani@gmail.com>
2020-11-12 14:08:19 -0500

    commit tree 93dfaf7b8a7611a484c35c118b159f509a702e4e

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  num_ls = []
+  while i < array.length
+    num_ls << "#{i + 1}. #{array[i]}"
+    i += 1
+  end
+  puts num_ls
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|i| i.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |element|
+    cheese_types.include?(element)
+  end
 end
